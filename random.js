f8fe02e7bc122edc45a3b3d7f16d80bf4f4954c3
sadat <sadat.himel@gmail.com>
2020-07-18 03:31:40 +0600

    random number problem solution

---
+++ random.js
@@ -0,0 +1,15 @@
+var num = 2.49999949493;
+var result = Math.floor(num);
+var result2 = Math.ceil(num);
+var result3 = Math.round(num);
+
+for(var i = 0; i<10; i++){
+var randonNum = Math.random() * 6;
+var output = Math.round(randonNum);
+console.log(output);
+}
+
+// console.log(result);
+// console.log(result2);
+// console.log(result3);
+
