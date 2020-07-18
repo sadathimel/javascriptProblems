7e5cad8b19877506e0f6839e5a8d14544315af09
sadat <sadat.himel@gmail.com>
2020-07-18 12:05:02 +0600

    add some arry  with for loop and function

---
+++ arraySome.js
@@ -0,0 +1,14 @@
+
+
+
+function arraySome(n) {
+  var sum = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    var element = numbers[i];
+    sum = sum + element;
+  }
+  return sum;
+}
+var numbers = [45, 65, 78, 12, 3, 54, 65];
+var result = arraySome(numbers);
+console.log("total of the numbers: ", result);
