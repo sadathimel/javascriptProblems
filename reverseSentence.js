e6ad4f0eb85ed96ad5047981b9dab768a94e1e40
sadat <sadat.himel@gmail.com>
2020-07-18 13:08:18 +0600

    reverse string from a straing

---
+++ reverseSentence.js
@@ -0,0 +1,11 @@
+function reverseString (str){
+      var revrese = "";
+      for(var i = 0; i<str.length; i++){
+            var char = str[i];
+            revrese = char + revrese;
+      }
+      return revrese;
+}
+var statement = "Hello Alien bhai brother.";
+var forAlien = reverseString(statement);
+console.log(forAlien);
